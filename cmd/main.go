d225905e8f7ba0948b13cfeb6815ef3b0a3d6da3
jython1 <jython1@gmail.com>
2026-02-23 13:59:25 +0300

    Created config.yaml

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server ServerConfig `yaml:"server"`
+}
+
+type ServerConfig struct {
+	Port int `yaml:"port"`
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("I love Milana!!!"))
-	})
+	var configPath string
+	pflag.StringVarP(&configPath, "config", "c", "config.yaml", "path to config file")
+	pflag.Parse()
 
-	http.ListenAndServe(":8080", nil)
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatalf("Cannot read config: %v", err)
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+
+	if err != nil {
+		log.Fatalf("Cannot parse config: %v", err)
+	}
+
+	addr := fmt.Sprintf(":%d", cfg.Server.Port)
+	fmt.Printf("Server starting on Port: %d: ", cfg.Server.Port)
+
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -10,4 +10,6 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
